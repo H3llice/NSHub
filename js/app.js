ad1640070f99652f1115041043dad2b1a1590787
alouguim <alexandredouglas16@gmail.com>
2026-08-17 08:48:41 -0300

    frontend das solicitacoes de compra e criação da aba compras

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import { inicializarEstoque } from './modules/estoque.js'
 import { inicializarClientes, inicializarContratos } from './modules/contratos.js'
 import { inicializarContasReceber, renderizarDashboardInicio } from './modules/pagamentos.js'
 import { inicializarFornecedores } from './modules/fornecedores.js'
+import { inicializarSolicitacoes } from './modules/solicitacoes.js'
 
 
 let favoritos = JSON.parse(localStorage.getItem('favoritos')) || [];
@@ -91,6 +92,10 @@ window.abrirPagina = function (event, id) {
     if (id === 'fornecedores') {
         inicializarFornecedores()
     }
+
+    if (id === 'solicitacoes') {
+    inicializarSolicitacoes()
+}
 }
 
 // Resetar sidebar quando redimensionar
